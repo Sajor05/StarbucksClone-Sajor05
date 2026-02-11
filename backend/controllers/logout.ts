ee533e18dbc13f7cc65da105ec090c03c10d5ad3
Fabrizio <alexandersajor05@gmail.com>
2026-02-11 00:19:55 -0300

    Logout actualizado

--- backend/controllers/logout.ts
+++ backend/controllers/logout.ts
@@ -1,10 +1,11 @@
 import type { Request, Response } from "express";
 
 export async function logout(req: Request, res: Response) {
+  const isProduction = process.env.NODE_ENV === "production";
   res.clearCookie("token", {
     httpOnly: true,
-    secure: true,
-    sameSite: "strict",
+    secure: isProduction ? true : false,
+    sameSite: isProduction ? "none" : "lax",
     path: "/",
   });
   return res.status(200).json({ message: "Sesión cerrada exitosamente" });
